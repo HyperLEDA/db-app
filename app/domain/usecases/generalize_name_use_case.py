858a2f4b3b106c8fc68ef2ff396fd5764320e5ca
d-makarov-d <danila.makarov@gmail.com>
2024-04-09 03:16:46 +0300

    Initial tests for GeneralizeNameUseCase

---
+++ app/domain/usecases/generalize_name_use_case.py
@@ -0,0 +1,6 @@
+class GeneralizeNameUseCase:
+    """
+    Given arbitrary name, transforms it to standard representation (removing abbreviation, trailing zeros, etc)
+    """
+    def invoke(self, source: str) -> str:
+        pass
