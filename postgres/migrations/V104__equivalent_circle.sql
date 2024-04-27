04df5fb16ed998c35792c50c2e8ff9361c5b66d7
dimakarov <66037161+dimakarov@users.noreply.github.com>
2024-04-27 16:58:58 +0300

    #121: some additions to equivCircle schema (#123)
    * #121: renamed and reordered files
    
    * #121: added circle table into equivCircle schema

--- postgres/migrations/V104__equivalent_circle.sql
+++ postgres/migrations/V104__equivalent_circle.sql
@@ -10,8 +10,30 @@ CREATE SCHEMA equivCircle ;
 COMMENT ON SCHEMA equivCircle	IS 'Equivalent Circle catalog' ;
 
 
------- Equivalent circle corresponding to the specific total flux level --------
+------ Circle shape --------
 CREATE TABLE equivCircle.circle (
+  pgc	integer	NOT NULL	REFERENCES common.pgc (id )	ON DELETE restrict ON UPDATE cascade
+, dataset	integer	NOT NULL	REFERENCES photometry.dataset (id)	ON DELETE restrict ON UPDATE cascade
+, band	integer	REFERENCES common.calibpassband (id)	ON DELETE restrict ON UPDATE cascade
+, a	real	NOT NULL	CHECK (a>0)
+, e_a	real	CHECK (e_a>0)
+, quality	common.quality	NOT NULL	DEFAULT 'ok'
+, modification_time	timestamp without time zone	NOT NULL	DEFAULT now()
+, PRIMARY KEY (pgc,dataset,band)
+) ;
+
+COMMENT ON TABLE equivCircle.circle	IS 'Equivalent Circle' ;
+COMMENT ON COLUMN equivCircle.circle.pgc	IS 'PGC number of the object' ;
+COMMENT ON COLUMN equivCircle.circle.dataset	IS 'Dataset of the measurements' ;
+COMMENT ON COLUMN equivCircle.circle.band	IS 'Passband ID' ;
+COMMENT ON COLUMN equivCircle.circle.a	IS 'Circle diameter [arcsec]' ;
+COMMENT ON COLUMN equivCircle.circle.e_a	IS 'Error of the diameter [arcsec]' ;
+COMMENT ON COLUMN equivCircle.circle.quality	IS 'Measurement quality' ;
+COMMENT ON COLUMN equivCircle.circle.modification_time	IS 'Timestamp when the record was added to the database' ;
+
+
+------ Equivalent circle corresponding to the specific total flux level --------
+CREATE TABLE equivCircle.fluxLevelCircle (
   magid	bigint	NOT NULL	REFERENCES photometry.totalMag (id)	ON DELETE restrict	ON UPDATE cascade
 , level	real	NOT NULL	CHECK (level>0 and level<100)
 , a	real	NOT NULL	CHECK (a>0)
@@ -19,11 +41,11 @@ CREATE TABLE equivCircle.circle (
 , UNIQUE (magid,level)
 ) ;
 
-COMMENT ON TABLE equivCircle.circle	IS 'Equivalent Circle at specific flux level' ;
-COMMENT ON COLUMN equivCircle.circle.magid	IS 'Totoal magnitude ID' ;
-COMMENT ON COLUMN equivCircle.circle.level	IS 'Level of the total flux [percent]' ;
-COMMENT ON COLUMN equivCircle.circle.a	IS 'Circle diameter [arcsec]' ;
-COMMENT ON COLUMN equivCircle.circle.e_a	IS 'Error of the diameter [arcsec]' ;
+COMMENT ON TABLE equivCircle.fluxLevelCircle	IS 'Equivalent Circle at specific flux level' ;
+COMMENT ON COLUMN equivCircle.fluxLevelCircle.magid	IS 'Totoal magnitude ID' ;
+COMMENT ON COLUMN equivCircle.fluxLevelCircle.level	IS 'Level of the total flux [percent]' ;
+COMMENT ON COLUMN equivCircle.fluxLevelCircle.a	IS 'Circle diameter [arcsec]' ;
+COMMENT ON COLUMN equivCircle.fluxLevelCircle.e_a	IS 'Error of the diameter [arcsec]' ;
 
 
 ------ Data Collection --------
@@ -43,10 +65,33 @@ SELECT
 , src.datatype
 , src.table_name
 FROM
-  equivCircle.circle	AS circ
+  equivCircle.fluxLevelCircle	AS circ
   LEFT JOIN photometry.totalMag	AS mag	ON (circ.magid=mag.id)
   LEFT JOIN photometry.dataset	AS ds	ON (mag.dataset=ds.id)
   LEFT JOIN rawdata.tables	AS src	ON (ds.src=src.id)
+
+UNION
+
+SELECT
+  circ.pgc
+, circ.dataset
+, ds.method
+, circ.band
+, NULL::real	AS level
+, circ.a
+, circ.e_a
+, circ.quality
+, circ.modification_time
+, ds.src
+, src.bib
+, src.datatype
+, src.table_name
+FROM
+  equivCircle.circle	AS circ
+  LEFT JOIN photometry.dataset	AS ds	ON (circ.dataset=ds.id)
+  LEFT JOIN rawdata.tables	AS src	ON (ds.src=src.id)
+;
+
 ;
 
 COMMENT ON VIEW equivCircle.data	IS 'Equivalent Circle catalog' ;
