3feac8a2078189deb5d0450524064c3daddfacec
Artyom Zaporozhets <kraysent@gmail.com>
2024-05-04 23:52:00 +0300

    remove noop authenticator

--- app/lib/auth/authenticator.py
+++ app/lib/auth/authenticator.py
@@ -7,18 +7,6 @@ from app.lib.auth import interface, user
 from app.lib.storage import postgres
 
 
-class NoopAuthenticator(interface.Authenticator):
-    """
-    This is a testing authenticator that successfully authenticates all users as admins.
-    """
-
-    def login(self, username: str, password: str) -> tuple[str, bool]:
-        return "noop_token", True
-
-    def authenticate(self, token: str) -> tuple[user.User, bool]:
-        return user.User(1, user.Role.ADMIN), True
-
-
 class PostgresAuthenticator(interface.Authenticator):
     """
     Authenticates users and stores authentication data in Postgres database.
