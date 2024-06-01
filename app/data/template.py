250634539269a1f71952b9dd36ab966e9bd1282e
Artyom Zaporozhets <kraysent@gmail.com>
2024-06-01 17:34:29 +0300

    removed unused templates

--- app/data/template.py
+++ app/data/template.py
@@ -1,7 +1,5 @@
 import jinja2
 
-# TODO: make a unified way for querying with templates
-
 
 def render_query(query_string: str, **kwargs) -> str:
     tpl = jinja2.Environment(loader=jinja2.BaseLoader()).from_string(query_string)
@@ -13,7 +11,6 @@ ONE_BIBLIOGRAPHY = """
 SELECT id, bibcode, year, author, title FROM common.bib
 WHERE id = %s
 """
-
 
 BIBLIOGRAPHY_TEMPLATE = """
 SELECT
@@ -37,30 +34,6 @@ NEW_OBJECTS = """
 INSERT INTO common.pgc
 VALUES {% for _ in range(n) %}(DEFAULT){% if not loop.last %},{% endif %}{% endfor %}
 RETURNING id;
-"""
-
-NEW_DESIGNATIONS = """
-INSERT INTO designation.data (pgc, design, bib)
-VALUES
-    {% for obj in objects %}
-    (%s, %s, %s){% if not loop.last %},{% endif %}
-    {% endfor %}
-"""
-
-NEW_COORDINATES = """
-INSERT INTO icrs.data (pgc, ra, dec, bib)
-VALUES
-    {% for obj in objects %}
-    (%s, %s, %s, %s){% if not loop.last %},{% endif %}
-    {% endfor %}
-"""
-
-GET_DESIGNATIONS = """
-SELECT design, bib, modification_time
-FROM designation.data
-WHERE pgc = %s
-OFFSET %s
-LIMIT %s
 """
 
 CREATE_TABLE = """
