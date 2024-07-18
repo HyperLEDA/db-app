6719f8ed7cc4237837b80852048ad16b015050c1
Artyom Zaporozhets <kraysent@gmail.com>
2024-07-18 10:28:17 +0300

    added env vars to remote

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -4,5 +4,6 @@ scp docker-compose.yaml $HOST:~/hyperleda/docker-compose.yaml
 ssh $HOST "rm -rf ~/hyperleda/configs"
 scp -r configs/ $HOST:~/hyperleda/configs
 scp -r ../configs/ $HOST:~/hyperleda
+scp .env.remote $HOST:~/hyperleda/.env.local
 
-ssh $HOST "cd ~/hyperleda && docker compose up -d"
+ssh $HOST "cd ~/hyperleda && set -a && . .env.local && set +a && docker compose up -d"
