c9fbed325ca26f18eed809b109c19d6f2c2d675c
Artyom Zaporozhets <kraysent@gmail.com>
2024-10-12 17:44:48 +0300

    #206: added serialization interface and moved some catalogues to layer 1

---
+++ app/data/repositories/layer1_repository.py
@@ -0,0 +1,12 @@
+from typing import final
+
+import structlog
+
+from app.lib.storage import postgres
+
+
+@final
+class Layer1Repository(postgres.TransactionalPGRepository):
+    def __init__(self, storage: postgres.PgStorage, logger: structlog.stdlib.BoundLogger) -> None:
+        self._logger = logger
+        super().__init__(storage)
