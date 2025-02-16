d31a9010d1ce1f935dd2a7d5e5592797b8e0ede0
Artyom Zaporozhets <kraysent@gmail.com>
2025-02-16 19:38:13 +0300

    added basic crossmatch algorithm

--- app/domain/processing/__init__.py
+++ app/domain/processing/__init__.py
@@ -1,3 +1,4 @@
+from app.domain.processing.interface import CI, ICRSCI, DesignationCI
 from app.domain.processing.mark_objects import mark_objects
 
-__all__ = ["mark_objects"]
+__all__ = ["CI", "DesignationCI", "ICRSCI", "mark_objects"]
