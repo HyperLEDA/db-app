3e869ef5cabbfecf9cb3ae1091d339c3ba7a8b40
Artyom Zaporozhets <kraysent@gmail.com>
2025-02-21 20:52:36 +0300

    fix name

--- app/data/repositories/__init__.py
+++ app/data/repositories/__init__.py
@@ -2,7 +2,7 @@ from app.data.repositories.common import CommonRepository
 from app.data.repositories.layer0 import INTERNAL_ID_COLUMN_NAME, Layer0Repository
 from app.data.repositories.layer1 import Layer1Repository
 from app.data.repositories.layer2 import Layer2Repository
-from app.data.repositories.queue import QueueRepository
+from app.data.repositories.queues import QueueRepository
 from app.data.repositories.tmp_data import TmpDataRepositoryImpl
 
 __all__ = [
