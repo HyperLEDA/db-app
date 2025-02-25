b9ced63b80e92e576d5e3e9e765dd00142caad63
Artyom Zaporozhets <kraysent@gmail.com>
2025-02-25 16:32:07 +0300

    272: added deploy script

--- infra/scripts/deploy.sh
+++ infra/scripts/deploy.sh
@@ -1,9 +1,10 @@
 HOST=$HOST
 BACKEND_USER=$BACKEND_USER
 
-cd ../
-scp infra/docker-compose.yaml $BACKEND_USER@$HOST:~/hyperleda/docker-compose.yaml
 ssh $BACKEND_USER@$HOST "rm -rf ~/hyperleda/configs"
+
+scp infra/docker-compose.yaml $BACKEND_USER@$HOST:~/hyperleda/docker-compose.yaml
+scp -r postgres/ $BACKEND_USER@$HOST:~/hyperleda/postgres
 scp -r infra/configs/ $BACKEND_USER@$HOST:~/hyperleda/configs
 scp -r configs/ $BACKEND_USER@$HOST:~/hyperleda
 scp infra/.env.remote $BACKEND_USER@$HOST:~/hyperleda/.env.local
