25bb0e4b3f613302072a23d69ae81aad8e874b2b
Artyom Zaporozhets <36627161+Kraysent@users.noreply.github.com>
2025-05-09 22:45:52 +0300

    #296: add homogenization documentation & cleanup docs (#298)
    * #296: add homogenization documentation & cleanup docs
    
    * update docs

--- app/presentation/adminapi/create_marking.py
+++ app/presentation/adminapi/create_marking.py
@@ -31,7 +31,7 @@ class CatalogSchema(GenericSchema[interface.CatalogToMark]):
         required=True,
         metadata={
             "description": "Map of parameter names to their configurations",
-            "example": {},
+            "example": {"ra": {"column_name": "ra"}, "dec": {"column_name": "dec"}},
         },
         keys=fields.String(),
         values=fields.Nested(ParameterSchema),
@@ -42,7 +42,7 @@ class CatalogSchema(GenericSchema[interface.CatalogToMark]):
         required=False,
         metadata={
             "description": "Additional parameters for the catalog",
-            "example": {},
+            "example": {"e_ra": 0.1, "e_dec": 0.2},
         },
     )
 
@@ -59,7 +59,84 @@ class CreateMarkingResponseSchema(Schema):
 async def create_marking_handler(actions: interface.Actions, r: web.Request) -> responses.APIOkResponse:
     """---
     summary: New marking rules for the table
-    description: Creates new marking rules to map the columns in the table to catalog parameters.
+    description: |
+        Creates new marking rules to map the columns in the table to catalog parameters. For a given table a marking
+        would consist of the mapping between catalog parameters and the columns from the original table.
+
+        For example, if one wants to create a marking for a column `object_name` that designates the name of an object,
+        they should create a catalog entry similar to the following:
+        ```json
+        {
+            "name": "designation",
+            "parameters": {
+                "design": {
+                    "column_name": "object_name"
+                }
+            }
+        }
+        ```
+
+        Here, `name` respresents the name of the catalog, keys of `parameters` map are parameter names and `column_name`
+        values are actual names of the columns under question. **For now, only one column per parameter is supported.**
+
+        It is possible to create several catalog entries for a single object, for example - is there are two columns
+        that represent a name of an object. In that case we might want to upload both names to the database so it is
+        easier to cross-identify and search these objects later. Another use case might be if there are several columns
+        that represent photometric information in different filters. In that case one might want to create one entry
+        for each of the magnitude columns.
+
+        In that case you can specify several entries into `rules` list with different values of `key`. For example:
+
+        ```json
+        {
+            "table_name": "my_table",
+            "rules": [
+                {
+                    "name": "designation",
+                    "parameters": {
+                        "design": {
+                            "column_name": "object_name"
+                        }
+                    },
+                    "key": "primary_name"
+                },
+                {
+                    "name": "designation",
+                    "parameters": {
+                        "design": {
+                            "column_name": "secondary_object_name"
+                        }
+                    },
+                    "key": "secondary_name"
+                }
+            ]
+        }
+        ```
+
+        The result of this would be two entries into the `designation` catalog for each object in the original table.
+
+        This handler also supports additional parameters that are not present in the original table. For example, a
+        table might not have a separate column for astrometric errors but from other sources you know that its error is
+        0.1 degrees for right ascension and 0.5 degrees for declination. You can specify this in the
+        `additional_parameters` field for each catalog:
+
+        ```json
+        {
+            "name": "icrs",
+            "parameters": {
+                "ra": {
+                    "column_name": "RAJ2000"
+                },
+                "dec": {
+                    "column_name": "DEJ2000"
+                }
+            },
+            "additional_parameters": {
+                "e_ra": 0.1,
+                "e_dec": 0.5
+            }
+        }
+        ```
     security:
         - TokenAuth: []
     tags: [table]
---
+++ docs/administrator/openapi.md
@@ -0,0 +1,6 @@
+Найти само API можно по ссылкам:
+
+- Тестовое окружение: [http://89.169.133.242/admin/api/docs](http://89.169.133.242/admin/api/docs)
+- Пользовательское окружение: [http://dm2.sao.ru:81/admin/api/docs](http://dm2.sao.ru:81/admin/api/docs)
+
+[OAD(docs/gen/swagger.json)]
