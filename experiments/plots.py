4e0e63da81bfb7655d3839ed5bc47fa0d6848ff7
Artyom Zaporozhets <kraysent@gmail.com>
2025-07-27 16:01:59 +0300

    add probabilities & refactor file saving

--- experiments/plots.py
+++ experiments/plots.py
@@ -7,8 +7,7 @@ from experiments import entities
 
 
 def get_hips_map(
-    ra: float,
-    dec: float,
+    obj: entities.CrossIDInfo,
     fov: float,
     hips_map: str,
     collision_data: list[entities.PGCObjectInfo],
@@ -18,8 +17,8 @@ def get_hips_map(
         hips=hips_map,
         width=500,
         height=500,
-        ra=Longitude(ra * u.deg),
-        dec=Latitude(dec * u.deg),
+        ra=Longitude(obj.ra * u.deg),
+        dec=Latitude(obj.dec * u.deg),
         fov=Angle(fov * u.deg),
         projection="AIT",
         get_query_payload=False,
@@ -29,8 +28,10 @@ def get_hips_map(
         cmap="viridis",
     )
     fov = fov * 0.5
-    ax.imshow(result, extent=(ra + fov, ra - fov, dec - fov, dec + fov))
-    ax.scatter(ra, dec, color="black", marker="x")
+    ax.imshow(result, extent=(obj.ra + fov, obj.ra - fov, obj.dec - fov, obj.dec + fov))
+    ax.scatter(obj.ra, obj.dec, color="blue", marker="x")
+    ax.errorbar(obj.ra, obj.dec, obj.pos_err, obj.pos_err, ecolor="blue")
+    ax.set_title(obj.name)
 
     for pgc_obj in collision_data:
         print(pgc_obj.ra, pgc_obj.dec, pgc_obj.name, pgc_obj.pos_err)
@@ -38,19 +39,20 @@ def get_hips_map(
         ax.errorbar(pgc_obj.ra, pgc_obj.dec, pgc_obj.pos_err, pgc_obj.pos_err, ecolor="red")
         ax.text(pgc_obj.ra, pgc_obj.dec, pgc_obj.name, color="white", fontsize=8, ha="left", va="bottom")
 
-        ax.plot([ra, pgc_obj.ra], [dec, pgc_obj.dec], color="yellow", linewidth=1, alpha=0.7)
+        ax.plot([obj.ra, pgc_obj.ra], [obj.dec, pgc_obj.dec], color="yellow", linewidth=1, alpha=0.7)
 
-        central_coord = SkyCoord(ra=ra * u.deg, dec=dec * u.deg)
+        central_coord = SkyCoord(ra=obj.ra * u.deg, dec=obj.dec * u.deg)
         collision_coord = SkyCoord(ra=pgc_obj.ra * u.deg, dec=pgc_obj.dec * u.deg)
         distance = central_coord.separation(collision_coord)
 
-        mid_ra = (ra + pgc_obj.ra) / 2
-        mid_dec = (dec + pgc_obj.dec) / 2
+        mid_ra = (obj.ra + pgc_obj.ra) / 2
+        mid_dec = (obj.dec + pgc_obj.dec) / 2
 
+        prob = obj.pgc_numbers[pgc_obj.pgc]
         ax.text(
             mid_ra,
             mid_dec,
-            f'{distance.arcsec:.1f}"',
+            f'{distance.arcsec:.1f}" | {prob:.3f}',
             color="yellow",
             fontsize=8,
             ha="center",
