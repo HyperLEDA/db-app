a2b2d569f69ab96c8fa290854efe74f1e9e8db5f
Artyom Zaporozhets <kraysent@gmail.com>
2025-09-14 13:31:47 +0300

    remove client usage from regression test

--- tests/regression/upload_simple_table.py
+++ tests/regression/upload_simple_table.py
@@ -3,36 +3,38 @@ import random
 import time
 import uuid
 
-import hyperleda
 import pandas
+import requests
 
 from app.commands.runtask import RunTaskCommand
 from app.lib import commands
+from app.lib.storage import enums
+from app.presentation import adminapi
 from tests import lib
 
 random.seed(time.time())
 
 
 @lib.test_logging_decorator(__file__)
-def create_marking(client: hyperleda.HyperLedaClient, table_name: str):
-    client.create_marking(
+def create_marking(session: requests.Session, table_name: str):
+    request_data = adminapi.CreateMarkingRequest(
         table_name=table_name,
-        rules=[
-            hyperleda.CatalogToMark(
+        catalogs=[
+            adminapi.CatalogToMark(
                 name="icrs",
                 parameters={
-                    "ra": hyperleda.ParameterToMark(column_name="ra"),
-                    "dec": hyperleda.ParameterToMark(column_name="dec"),
-                    "e_ra": hyperleda.ParameterToMark(column_name="e_ra"),
-                    "e_dec": hyperleda.ParameterToMark(column_name="e_dec"),
+                    "ra": adminapi.ParameterToMark(column_name="ra"),
+                    "dec": adminapi.ParameterToMark(column_name="dec"),
+                    "e_ra": adminapi.ParameterToMark(column_name="e_ra"),
+                    "e_dec": adminapi.ParameterToMark(column_name="e_dec"),
                 },
                 key=None,
                 additional_params=None,
             ),
-            hyperleda.CatalogToMark(
+            adminapi.CatalogToMark(
                 name="designation",
                 parameters={
-                    "design": hyperleda.ParameterToMark(column_name="name"),
+                    "design": adminapi.ParameterToMark(column_name="name"),
                 },
                 key=None,
                 additional_params=None,
@@ -40,56 +42,67 @@ def create_marking(client: hyperleda.HyperLedaClient, table_name: str):
         ],
     )
 
+    response = session.post("/v1/marking", json=request_data.model_dump())
+    response.raise_for_status()
+
 
 @lib.test_logging_decorator(__file__)
-def create_bibliography(client: hyperleda.HyperLedaClient) -> str:
-    return client.create_internal_source(
+def create_bibliography(session: requests.Session) -> str:
+    request_data = adminapi.CreateSourceRequest(
         authors=["Doe, J."],
         title=str(uuid.uuid4()),
         year=2024,
     )
 
+    response = session.post("/v1/source", json=request_data.model_dump())
+    response.raise_for_status()
+    return response.json()["data"]["code"]
+
 
 @lib.test_logging_decorator(__file__)
-def create_table(client: hyperleda.HyperLedaClient, bib_id: str) -> tuple[int, str]:
+def create_table(session: requests.Session, bib_id: str) -> tuple[int, str]:
     table_name = f"test_{str(uuid.uuid4())}"
 
-    return client.create_table(
-        hyperleda.CreateTableRequest(
-            table_name=table_name,
-            columns=[
-                hyperleda.ColumnDescription(
-                    name="name", data_type=hyperleda.DatatypeEnum.str, ucd="meta.id", unit=None, description=None
-                ),
-                hyperleda.ColumnDescription(
-                    name="ra",
-                    data_type=hyperleda.DatatypeEnum.float,
-                    unit="hourangle",
-                    ucd="pos.eq.ra",
-                    description=None,
-                ),
-                hyperleda.ColumnDescription(
-                    name="dec", data_type=hyperleda.DatatypeEnum.float, unit="deg", ucd="pos.eq.dec", description=None
-                ),
-                hyperleda.ColumnDescription(
-                    name="e_ra", data_type=hyperleda.DatatypeEnum.float, unit="deg", ucd=None, description=None
-                ),
-                hyperleda.ColumnDescription(
-                    name="e_dec", data_type=hyperleda.DatatypeEnum.float, unit="deg", ucd=None, description=None
-                ),
-                hyperleda.ColumnDescription(
-                    name="fuzz", data_type=hyperleda.DatatypeEnum.str, ucd=None, unit=None, description=None
-                ),
-            ],
-            bibcode=bib_id,
-            datatype=hyperleda.DataType.regular,
-            description="",
-        )
-    ), table_name
+    request_data = adminapi.CreateTableRequest(
+        table_name=table_name,
+        columns=[
+            adminapi.ColumnDescription(
+                name="name", data_type=adminapi.DatatypeEnum["str"], ucd="meta.id", unit=None, description=None
+            ),
+            adminapi.ColumnDescription(
+                name="ra",
+                data_type=adminapi.DatatypeEnum["float"],
+                unit="hourangle",
+                ucd="pos.eq.ra",
+                description=None,
+            ),
+            adminapi.ColumnDescription(
+                name="dec", data_type=adminapi.DatatypeEnum["float"], unit="deg", ucd="pos.eq.dec", description=None
+            ),
+            adminapi.ColumnDescription(
+                name="e_ra", data_type=adminapi.DatatypeEnum["float"], unit="deg", ucd=None, description=None
+            ),
+            adminapi.ColumnDescription(
+                name="e_dec", data_type=adminapi.DatatypeEnum["float"], unit="deg", ucd=None, description=None
+            ),
+            adminapi.ColumnDescription(
+                name="fuzz", data_type=adminapi.DatatypeEnum["str"], ucd=None, unit=None, description=None
+            ),
+        ],
+        bibcode=bib_id,
+        datatype=enums.DataType.REGULAR,
+        description="",
+    )
+
+    response = session.post("/v1/table", json=request_data.model_dump())
+    response.raise_for_status()
+    table_id = response.json()["data"]["id"]
+
+    return table_id, table_name
 
 
 @lib.test_logging_decorator(__file__)
-def upload_data(client: hyperleda.HyperLedaClient, table_id: int):
+def upload_data(session: requests.Session, table_id: int):
     df = pandas.DataFrame.from_records(
         [
             {
@@ -111,7 +124,13 @@ def upload_data(client: hyperleda.HyperLedaClient, table_id: int):
         ]
     )
 
-    client.add_data(table_id, df)
+    request_data = adminapi.AddDataRequest(
+        table_id=table_id,
+        data=df.to_dict("records"),  # type: ignore
+    )
+
+    response = session.post("/v1/table/data", json=request_data.model_dump())
+    response.raise_for_status()
 
 
 @lib.test_logging_decorator(__file__)
@@ -134,12 +153,17 @@ def start_processing(table_id: int, table_name: str):
 
 
 @lib.test_logging_decorator(__file__)
-def get_object_statuses(client: hyperleda.HyperLedaClient, table_name: str) -> dict[str, int]:
-    table_info = client.get_table(table_name)
-    if table_info.statistics is None:
+def get_object_statuses(session: requests.Session, table_name: str) -> dict[str, int]:
+    request_data = adminapi.GetTableRequest(table_name=table_name)
+
+    response = session.get("/v1/table", params=request_data.model_dump())
+    response.raise_for_status()
+
+    table_info = response.json()["data"]
+    if table_info["statistics"] is None:
         raise ValueError("Processing status is None")
 
-    return table_info.statistics
+    return table_info["statistics"]
 
 
 @lib.test_logging_decorator(__file__)
@@ -158,15 +182,15 @@ def run():
     api_port = os.getenv("API_PORT", "8080")
     api_url = f"http://{api_host}:{api_port}"
 
-    client = hyperleda.HyperLedaClient(api_url)
-    code = create_bibliography(client)
-    table_id, table_name = create_table(client, code)
-    upload_data(client, table_id)
+    session = lib.TestSession(api_url)
+    code = create_bibliography(session)
+    table_id, table_name = create_table(session, code)
+    upload_data(session, table_id)
 
-    create_marking(client, table_name)
+    create_marking(session, table_name)
     start_processing(table_id, table_name)
 
-    statuses_data = get_object_statuses(client, table_name)
+    statuses_data = get_object_statuses(session, table_name)
     assert statuses_data["new"] == 2
 
     layer1_import(table_id)
