5e7b1fcffbec417ead00183fbec73b724f2c806d
Dmitry Makarov <dim@sao.ru>
2025-10-09 18:09:49 +0300

    PGC structure

--- postgres/drafts/00_pgc.sql
+++ postgres/drafts/00_pgc.sql
@@ -16,44 +16,55 @@ COMMENT ON TABLE common.pgc     IS 'The list of existing PGC-numbers' ;
 COMMENT ON COLUMN common.pgc.id IS 'Unique PGC-number' ;
 
 
+--------------------------
+-- Ассоциация записей с PGC номерами переносится в таблицу objects
+--------------------------
 
-
-
-DROP TABLE rawdata.pgc ;
-DROP FUNCTION rawdata.next_pgc ;
-
-ALTER TABLE rawdata.objects RENAME TO rawdata.records ;
-
-ALTER TABLE rawdata.records 
-ADD COLUMN pgc Integer REFERENCES common.pgc (pgc) ON DELETE restrict ON UPDATE cascade,
+ALTER TABLE rawdata.objects 
+ADD COLUMN pgc Integer REFERENCES common.pgc (id) ON DELETE restrict ON UPDATE cascade,
 ADD COLUMN modification_time Timestamp Without Time Zone ;
 
-COMMENT ON TABLE rawdata.records IS 'The register of all records in original data tables' ;
-COMMENT ON COLUMN rawdata.records.id  IS 'The record id' ;
-COMMENT ON COLUMN rawdata.records.table_id IS 'The table in which the record is located' ;
-COMMENT ON COLUMN rawdata.records.pgc IS 'Corssidentification of the record with the PGC-number' ;
-COMMENT ON COLUMN rawdata.records.modification_time IS 'Time of PGC-number assignment to the record' ;
-
---------------------------
--- Ассоциация записей с PGC номерами переносится в таблицу records
---------------------------
+COMMENT ON TABLE rawdata.objects IS 'The register of all objects in original data tables' ;
+COMMENT ON COLUMN rawdata.objects.id  IS 'The record id' ;
+COMMENT ON COLUMN rawdata.objects.table_id IS 'The table in which the record is located' ;
+COMMENT ON COLUMN rawdata.objects.pgc IS 'Corssidentification of the record with the PGC-number' ;
+COMMENT ON COLUMN rawdata.objects.modification_time IS 'Time of PGC-number assignment to the record' ;
 
 
+---------------------------
+-- Перенос данных
+---------------------------
 
-ALTER TABLE rawdata.tables DROP COLUMN datatype ;
+INSERT INTO common.pgc (id) 
+SELECT id 
+FROM rawdata.pgc 
+ORDER BY rawdata.pgc.id 
+;
+
+UPDATE rawdata.objects
+SET
+  pgc=rawdata.pgc.id
+FROM rawdata.pgc
+WHERE 
+  rawdata.pgc.object_id=rawdata.objects.id
+;
+
 
 ---------------------------
 -- Категория данных переносится в описание наборов данных
 ---------------------------
 
+ALTER TABLE rawdata.tables DROP COLUMN datatype ;
 
 
-ALTER TABLE rawdata.tables SET SCHEMA layer0 ;
-ALTER TABLE rawdata.records SET SCHEMA layer0 ;
+--------------------------------------
+-- В дальнейшем нужно будет
+-- 1. Перенести связь между записями и PGC в таблицу objects
+-- 2. Удалить таблицу rawdata.pgc
+-- 3. Перименовать таблицу objects в records
+-- 4. Перенести таблицы records и tables в схему layer0
+--------------------------------------
 
---------------------------
--- Служебные таблицы уровня 0 переносятся в схему laye0
---------------------------
-
-COMMIT;
+ROLLBACK;
+-- COMMIT;
 
