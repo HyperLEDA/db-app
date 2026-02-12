ea01cc976423024b86e60a199e52bb458a85c1c8
Dmitry Makarov <dim@sao.ru>
2026-02-12 18:25:00 +0300

    cz catalog

--- postgres/drafts/03_modify_cz.sql
+++ postgres/drafts/03_modify_cz.sql
@@ -1,10 +1,18 @@
 BEGIN;
 
-CREATE TYPE cz.DimentionType AS ENUM (0, 1, 2) ;
-COMMENT ON TYPE cz.DimentionType IS '0 corresponds to a beam/fiber, 1 corresponds to a long-slit, 2 corresponds to panoramic spectroscopy ' ;
+-- CREATE TYPE cz.DimentionType AS ENUM (0, 1, 2) ;
+-- COMMENT ON TYPE cz.DimentionType IS '0 corresponds to a beam/fiber, 1 corresponds to a long-slit, 2 corresponds to panoramic spectroscopy ' ;
 
 CREATE TYPE cz.MethodType AS ENUM ('average','emission','absorption','xcorr','fit','photoz') ;
-COMMENT ON TYPE cz.MethodType IS 'average - Average of measurements of membersi; emission - Emission lines; absorption - Absorption lines; xcorr - Cross correlation; fit - Stellar populations fit; photoz - Photometric redshift' ;
+
+COMMENT ON TYPE cz.MethodType IS '{ 
+"average": "Average of measurements of membersi",
+"emission": "Emission lines",
+"absorption": "Absorption lines",
+"xcorr": "Cross correlation",
+"fit": "Stellar populations fit",
+"photoz": "Photometric redshift"
+}' ;
 
 
 CREATE TABLE cz.datasets (
@@ -12,9 +20,7 @@ CREATE TABLE cz.datasets (
 , table_id Integer NOT NULL REFERENCES layer0.tables (id) ON DELETE restrict ON UPDATE cascade
 , column_name Text NOT NULL
 , datatype Common.DataType NOT NULL
-, fovdim cz.DimentionType
 , method cz.MethodType
-, resolution Real
 ) ;
 
 ALTER TABLE cz.data
@@ -30,9 +36,7 @@ SELECT
 , d.quality
 , s.datatype
 
-, s.fovdim
 , s.method
-, s.resolution
 
 , d.record_id
 , d.dataset_id
