cf8a837a72ebf53ac36887bdf9d46f7c54ea3589
kraysent <kraysent@gmail.com>
2026-03-12 12:00:14 +0000

    #414: designations layer 2 catalog

---
+++ postgres/migrations/V030__additional_designations.sql
@@ -0,0 +1,19 @@
+CREATE VIEW layer2.designations AS
+SELECT
+  r.pgc
+, d.design
+, t.bib
+, b.code
+, b.year
+, b.author
+, b.title
+FROM
+  designation.data AS d 
+  LEFT JOIN layer0.records AS r ON (d.record_id = r.id)
+  LEFT JOIN layer0.tables AS t ON (r.table_id = t.id)
+  LEFT JOIN common.bib AS b ON (t.bib = b.id)
+WHERE r.pgc IS NOT NULL;
+
+CREATE INDEX IF NOT EXISTS layer0_records_id_pgc_not_null
+ON layer0.records (id)
+WHERE pgc IS NOT NULL;
