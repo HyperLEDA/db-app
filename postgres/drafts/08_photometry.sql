760b0528fb35efad999aa55f0c1fa0a9457cfb7e
Dmitry Makarov <dim@sao.ru>
2026-03-15 20:21:58 +0300

    Isophote schema

--- postgres/drafts/08_photometry.sql
+++ postgres/drafts/08_photometry.sql
@@ -24,7 +24,7 @@ CREATE TYPE photometry.MagMethodType AS ENUM ( 'psf', 'visual', 'aperture', 'iso
 COMMENT ON TYPE photometry.MagMethodType IS '{ "psf":"Point Spread Function photometry for the point source", "visual":"Visual estimate", "aperture":"Aperture photometry", "isophotal":"Magnitude measurement inside a specified isophote", "asymptotic":"Asymptotic (extrapolated) magnitude when the aperture size -> Inf", "model":"Best fit model of the light distribution", "petrosian":"Petrosian adaptively scaled aperture", "kron":"Kron adaptively scaled aperture" }' ;
 
 
-CREATE TABLE photometry.total (
+CREATE TABLE photometry.data (
   record_id	Text	NOT NULL	REFERENCES layer0.records(id) ON UPDATE cascade ON DELETE restrict
 , band	text	NOT NULL	REFERENCES photometry.calib_bands (id) ON DELETE restrict ON UPDATE cascade
 , mag	real	NOT NULL	CHECK ( mag > -1 and mag < 30 )
@@ -32,35 +32,15 @@ CREATE TABLE photometry.total (
 , method	photometry.MagMethodType	NOT NULL	CHECK ( method IN ('psf', 'visual', 'asymptotic', 'model', 'petrosian', 'kron') )
 , PRIMARY KEY (record_id, method, band)
 );
-CREATE INDEX ON photometry.total (record_id) ;
-CREATE INDEX ON photometry.total (band) ;
-CREATE INDEX ON photometry.total (method) ;
+CREATE INDEX ON photometry.data (record_id) ;
+CREATE INDEX ON photometry.data (band) ;
+CREATE INDEX ON photometry.data (method) ;
 
-COMMENT ON TABLE photometry.total	IS 'Catalog of the total magnitudes';
-COMMENT ON COLUMN photometry.total.record_id	IS 'Record ID';
-COMMENT ON COLUMN photometry.total.band	IS 'Calibrated passband ID';
-COMMENT ON COLUMN photometry.total.mag	IS '{"description":"Total (PSF|model|asymptotic|etc.) magnitude", "unit":"mag", "ucd":"phot.mag"}';
-COMMENT ON COLUMN photometry.total.e_mag	IS '{"description":"Error of the total magnitude", "unit":"mag", "ucd":"stat.error;phot.mag"}';
-COMMENT ON COLUMN photometry.total.method	IS 'Photometry method type (PSF, Kron, etc.)' ;
-
-
-CREATE TABLE photometry.isophotal (
-  record_id	Text	NOT NULL	REFERENCES layer0.records(id) ON UPDATE cascade ON DELETE restrict
-, band	text	NOT NULL	REFERENCES photometry.calib_bands (id) ON DELETE restrict ON UPDATE cascade
-, mag	real	NOT NULL	CHECK ( mag > -1 and mag < 30 )
-, e_mag	real	CHECK ( e_mag > 0 and e_mag < 0.5 )
-, isophote	real	NOT NULL	CHECK ( mag > -1 and mag < 30 )
-, PRIMARY KEY (record_id, band, isophote)
-);
-CREATE INDEX ON photometry.isophotal (record_id) ;
-CREATE INDEX ON photometry.isophotal (band) ;
-CREATE INDEX ON photometry.isophotal (isophote) ;
-
-COMMENT ON TABLE photometry.isophotal	IS 'Catalog of the isophotal magnitudes';
-COMMENT ON COLUMN photometry.isophotal.record_id	IS 'Record ID';
-COMMENT ON COLUMN photometry.isophotal.band	IS 'Calibrated passband ID';
-COMMENT ON COLUMN photometry.isophotal.mag	IS '{"description":"Isophotal magnitude", "unit":"mag", "ucd":"phot.mag"}';
-COMMENT ON COLUMN photometry.isophotal.e_mag	IS '{"description":"Error of the isophotal magnitude", "unit":"mag", "ucd":"stat.error;phot.mag"}';
-COMMENT ON COLUMN photometry.isophotal.isophote	IS 'Isophote level' ;
+COMMENT ON TABLE photometry.data	IS 'Catalog of the total magnitudes';
+COMMENT ON COLUMN photometry.data.record_id	IS 'Record ID';
+COMMENT ON COLUMN photometry.data.band	IS '{"description":"Calibrated passband ID", "ucd":"instr.filter"}';
+COMMENT ON COLUMN photometry.data.mag	IS '{"description":"Total (PSF|model|asymptotic|etc.) magnitude", "unit":"mag", "ucd":"phot.mag"}';
+COMMENT ON COLUMN photometry.data.e_mag	IS '{"description":"Error of the total magnitude", "unit":"mag", "ucd":"stat.error;phot.mag"}';
+COMMENT ON COLUMN photometry.data.method	IS 'Photometry method type (PSF, Kron, etc.)' ;
 
 COMMIT;
