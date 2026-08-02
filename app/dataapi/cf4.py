3b1a1a366ae1d3e01ca0874e30ca91e1f06fdcb3
Eugene <esendzikas@yandex.ru>
2026-08-02 12:11:07 +0300

    cf4 fix

--- app/dataapi/cf4.py
+++ app/dataapi/cf4.py
@@ -1,16 +1,14 @@
 import astropy.units as u
-from astropy.coordinates import SkyCoord
 import numpy as np
+from astropy.coordinates import SkyCoord
 
 clight = 299792.458  # км/с
 
 
-def to_grid_index(coord, delta=1000 / 128, N=128, L=1000):
-    """Преобразует координату в индекс сетки"""
-    coord = (coord + L / 2)
-    index = np.floor(coord / delta).astype(int)
-    index = np.clip(index, 0, N - 1)
-    return index
+def to_grid_index(coord, delta=1000 / 128, n=128, length=1000):
+    """Преобразование координаты в индекс сетки"""
+    coord = coord + length / 2
+    return np.clip(np.floor(coord / delta).astype(int), 0, n - 1)
 
 
 def get_data_from_skycoord_redshift(skycoord, redshift):
@@ -36,8 +34,7 @@ def get_data_from_skycoord_redshift(skycoord, redshift):
     sgb = skycoord.supergalactic.sgb
 
     # Вычисляем картезианские координаты в супергалактической системе
-    sgc = SkyCoord(sgl=sgl, sgb=sgb, distance=distance_mpc * u.Mpc,
-                   frame='supergalactic')
+    sgc = SkyCoord(sgl=sgl, sgb=sgb, distance=distance_mpc * u.Mpc, frame="supergalactic")
 
     sgx = sgc.cartesian.x.value
     sgy = sgc.cartesian.y.value
@@ -75,8 +72,7 @@ def get_data_from_skycoord_velocity(skycoord, velocity_km_s):
     sgb = skycoord.supergalactic.sgb
 
     # Вычисляем картезианские координаты в супергалактической системе
-    sgc = SkyCoord(sgl=sgl, sgb=sgb, distance=distance_mpc * u.Mpc,
-                   frame='supergalactic')
+    sgc = SkyCoord(sgl=sgl, sgb=sgb, distance=distance_mpc * u.Mpc, frame="supergalactic")
 
     sgx = sgc.cartesian.x.value
     sgy = sgc.cartesian.y.value
@@ -93,32 +89,34 @@ def get_data_from_skycoord_velocity(skycoord, velocity_km_s):
 
 def _load_grid_data(ix, iy, iz):
     """Внутренняя функция для загрузки данных из сетки"""
-    data = np.load('CF4pp_mean_std_grids.npz')
+    data = np.load("CF4pp_mean_std_grids.npz")
 
-    d_data = data['d_mean_CF4pp']
-    derr_data = data['d_std_CF4pp']
-    vxyz_data = data['v_mean_CF4pp']
-    vxyz_err_data = data['v_std_CF4pp']
-    vr_data = data['vr_mean_CF4pp']
-    verr_data = data['vr_std_CF4pp']
+    d_data = data["d_mean_CF4pp"]
+    derr_data = data["d_std_CF4pp"]
+    vxyz_data = data["v_mean_CF4pp"]
+    vxyz_err_data = data["v_std_CF4pp"]
+    vr_data = data["vr_mean_CF4pp"]
+    verr_data = data["vr_std_CF4pp"]
 
-    return (d_data[ix, iy, iz],
-            derr_data[ix, iy, iz],
-            vxyz_data[:, ix, iy, iz],
-            vxyz_err_data[:, ix, iy, iz],
-            vr_data[ix, iy, iz],
-            verr_data[ix, iy, iz])
+    return (
+        d_data[ix, iy, iz],
+        derr_data[ix, iy, iz],
+        vxyz_data[:, ix, iy, iz],
+        vxyz_err_data[:, ix, iy, iz],
+        vr_data[ix, iy, iz],
+        verr_data[ix, iy, iz],
+    )
 
 
 # Примеры использования:
 if __name__ == "__main__":
     # Пример 1: Галактические координаты + красное смещение
-    gc = SkyCoord(l=120.5 * u.deg, b=45.2 * u.deg, frame='galactic')
+    gc = SkyCoord(l=120.5 * u.deg, b=45.2 * u.deg, frame="galactic")
     redshift = 0.025
     result1 = get_data_from_skycoord_redshift(gc, redshift)
 
     # Пример 2: Экваториальные координаты + скорость
-    eq = SkyCoord(ra=185.3 * u.deg, dec=-12.7 * u.deg, frame='icrs')
+    eq = SkyCoord(ra=185.3 * u.deg, dec=-12.7 * u.deg, frame="icrs")
     velocity = 7500  # км/с
     result2 = get_data_from_skycoord_velocity(eq, velocity)
 
