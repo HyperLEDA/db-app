71d3e4649bb9e369b16f02caed21d1ece9443f97
Dmitry Makarov <dim@sao.ru>
2026-08-19 22:47:59 +0300

    CZ: comments have been removed

--- postgres/migrations/V064__add_cz_method_and_quality.sql
+++ postgres/migrations/V064__add_cz_method_and_quality.sql
@@ -28,17 +28,4 @@ ALTER TABLE cz.data
 ALTER TABLE cz.data
   ADD PRIMARY KEY (record_id, method) ;
 
--- UPDATE cz.data SET method = 'average'  WHERE record_id IN (SELECT hyperleda_internal_id FROM rawdata.hyperleda_m000) ;
--- UPDATE cz.data SET method = 'best'     WHERE record_id IN (SELECT hyperleda_internal_id FROM rawdata."nearby_galaxy_catalog_2026-02-22") ;
--- UPDATE cz.data SET method = 'best'     WHERE record_id IN (SELECT hyperleda_internal_id FROM rawdata."priv_comm_Makarov_MGC43397") ;
--- UPDATE cz.data SET method = 'best'     WHERE record_id IN (SELECT hyperleda_internal_id FROM rawdata."50_mpc_2026-05-26") ;
--- UPDATE cz.data SET method = 'emission' WHERE record_id IN (SELECT hyperleda_internal_id FROM rawdata."VIII_73_hicat") ;
--- UPDATE cz.data SET method = 'emission' WHERE record_id IN (SELECT hyperleda_internal_id FROM rawdata."VIII_89_nhicat_2026-08-12") ;
--- UPDATE cz.data SET method = 'fit'      WHERE record_id IN (SELECT hyperleda_internal_id FROM rawdata."J_A+A_660_A2_Alcyoneus") ;
-
--- DELETE FROM cz.data WHERE record_id IN (SELECT hyperleda_internal_id FROM rawdata."manga_2026-02-28") ;
-
--- UPDATE cz.data SET quality = 'suspected' WHERE record_id = '7e2c9a55-8b2a-6c6e-0c3e-2f48dbe9be5a' ;
-
--- ROLLBACK ;
 COMMIT;
