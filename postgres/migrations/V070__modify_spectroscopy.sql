2a5061c86f847f27a68d3cda6e2e2e60ed09789e
Dmitry Makarov <dim@sao.ru>
2026-08-27 21:26:27 +0300

    Modify spectroscopy measurement type

---
+++ postgres/migrations/V070__modify_spectroscopy.sql
@@ -0,0 +1,18 @@
+/* pgmigrate-encoding: utf-8 */
+BEGIN;
+
+ALTER TYPE spectroscopy.flux_method_type  ADD VALUE IF NOT EXISTS 'average' ;
+ALTER TYPE spectroscopy.flux_method_type  ADD VALUE IF NOT EXISTS 'unknown' ;
+
+COMMENT ON TYPE spectroscopy.flux_method_type IS '{
+"description": "Method of the integrated line flux measurement",
+"values": {
+  "sum": "Integrated flux density of the line determined by summing all spectral channels",
+  "gauss": "Line flux approximated by gaussian profile",
+  "busy": "Profile of the line approximated by busy function",
+  "average": "Average of several measurements",
+  "unknown": "Unspecified measurement"
+  }
+}';
+
+COMMIT ;
