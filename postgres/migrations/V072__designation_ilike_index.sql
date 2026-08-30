b1d8d9b0518ce52e0663832f5d6c7d417068f74f
kraysent <kraysent@gmail.com>
2026-08-30 14:59:48 +0100

    fix designation index

--- postgres/migrations/V072__designation_ilike_index.sql
+++ postgres/migrations/V072__designation_ilike_index.sql
@@ -1,2 +1,6 @@
+/* pgmigrate-encoding: utf-8 */
+
 CREATE INDEX IF NOT EXISTS designation_data_design_trgm_idx
   ON designation.data USING GIN (design gin_trgm_ops);
+
+DROP INDEX IF EXISTS layer2.layer2_designation_design_trgm_idx;
