6d6e41241d95084b17ad855b95ee5d3c9dc5f675
Dmitry Makarov <dim@sao.ru>
2026-08-31 15:50:51 +0300

    Set resolution>=0

--- postgres/migrations/V073__modify_resolution_check.sql
+++ postgres/migrations/V073__modify_resolution_check.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
 ALTER TABLE kinematics.line_width	DROP CONSTRAINT line_width_resolution_check ;
-ALTER TABLE kinematics.line_width	ADD CHECK (resolution > 0) ;
+ALTER TABLE kinematics.line_width	ADD CHECK (resolution >= 0) ;
 
 COMMIT;
